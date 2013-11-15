57dee1b5035bf98d47eabf501bd814e31444e241
bizzguy <jamesharmon@gmail.com>
2013-11-15 11:12:16 -0800

    changes made during class - including slides

--- 3-ImageCaptionView/src/com/example/imagecaptionview3/MainActivity.java
+++ 3-ImageCaptionView/src/com/example/imagecaptionview3/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		Log.wtf("tag","message");
 	}
 
